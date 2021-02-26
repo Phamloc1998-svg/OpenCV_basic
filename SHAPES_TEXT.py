27907b1a087cbb1674148745eb8ff46011538318
= <57940233+Phamloc1998-svg@users.noreply.github.com>
2021-02-26 22:24:48 +0700

    change shapes

--- SHAPES_TEXT.py
+++ SHAPES_TEXT.py
@@ -10,7 +10,7 @@ cv2.line(img,(0,0),(img.
 shape[1], img.shape[0]),(255,255,0),3)
 cv2.rectangle(img,(100,100),(250,250),(0,0,255),2)
 cv2.circle(img,(100,100),10,(255,255,0),3)
-cv2.putText(img,"Loveyou",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),3)
+cv2.putText(img,"LoveNgoc",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),3)
 
 cv2.imshow("Image",img)
 cv2.waitKey(0)
