dbc0248484fa7d0dda2e91bc5e2e57d359753c62
Phamloc1998-svg <locngoanhien168@gmail.com>
2021-03-12 17:07:28 +0700

    add

--- READ_IMAGE.py
+++ READ_IMAGE.py
@@ -2,5 +2,5 @@ import cv2
 # LOAD AN IMAGE USING 'IMREAD'
 img = cv2.imread ('ngoc.jpg')
 #DISPLAY
-cv2.imshow('ngociudau', img)
+cv2.imshow('ngoc', img)
 cv2.waitKey(0)
--- READ_WEBCAM.py
+++ READ_WEBCAM.py
@@ -1,12 +1,18 @@
+# import cv2
+# Width = 640 
+# Height = 480
+# cap = cv2.VideoCapture(0)
+# cap.set(3, Width)
+# cap.set(4, Height)
+# cap.set(10, 150)
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("loc",img)
+#     if cv2.waitKey(1) and 0xff == ord('q'):Q
+#         break
 import cv2
-Width = 640 
+Width = 640
 Height = 480
 cap = cv2.VideoCapture(0)
 cap.set(3, Width)
-cap.set(4, Height)
-cap.set(10, 150)
-while True:
-    success, img = cap.read()
-    cv2.imshow("loc",img)
-    if cv2.waitKey(1) and 0xff == ord('q'):
-        break
+cap.set(4, )
